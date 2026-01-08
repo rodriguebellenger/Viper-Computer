146497c4933ad2af023070db7944c4af0bc9ad19
rodriguebellenger <rodrigue.bellenger@gmail.com>
2026-01-08 09:54:46 +0100

    Commands

--- assembler/assembler.go
+++ assembler/assembler.go
@@ -231,26 +231,27 @@ func main() {
 //////////////
 
 func runCommand(args []string) {
-	content, err := os.ReadFile(args[0])
-	if err != nil {
-		log.Fatal("\rCouldn't read file : " + args[0])
-	}
-	content = content[1:]
+	var program string = readFile(args[0])
+	args = args[1:]
 	var debug bool = false
-	var time_measurement bool = false
+	var time_measurement uint64 = 1
 
-	for _, item := range args {
-		if item == "debug" {
+	for i := 0; i < len(args); i++ {
+		if args[i] == "debug" {
 			debug = true
-		} else if item == "-time" {
-			time_measurement = true
-		} else if isInt(item) {
+		} else if args[i] == "-time" {
+			if !isInt(args[i+1]) {
+				log.Fatal("-time needs a integer.")
+			} else if args[i+1][0] == '-' {
+				log.Fatal("-time needs a positive integer.")
+			}
+			time_measurement = uint64(strToInt(args[i+1]))
+			i += 1
 		} else {
-			log.Fatal("Unrecognized argument for run command : " + item)
+			log.Fatal("Unrecognized argument for run command : " + args[i])
 		}
 	}
 
-	var program string = string(content)
 	var assemblerProgram [][]string = readProgram(program)
 
 	var startTime time.Time = time.Now()
@@ -258,20 +259,37 @@ func runCommand(args []string) {
 	var elapsed time.Duration = time.Since(startTime)
 	if debug {
 		fmt.Println(byteProgram)
-		fmt.Printf("Temps : %s\n\n", elapsed)
+		fmt.Printf("Time : %s\n\n", elapsed)
 	}
-	if time_measurement {
-
+	if time_measurement == 1 {
+		startTime = time.Now()
+		executeProgram()
+		elapsed = time.Since(startTime)
+		if debug {
+			fmt.Printf("Time : %s\n", elapsed)
+		}
+	} else {
+		var total_time time.Duration
+		for i := 0; uint64(i) < time_measurement; i++ {
+			startTime = time.Now()
+			for i := 0; i < 1; i++ {
+				executeProgram()
+			}
+			total_time += time.Since(startTime)
+		}
+		fmt.Printf("Time : %s\n", total_time/200)
+		fmt.Printf("Total time : %s\n", total_time)
 	}
 }
 
 func checkCommand(args []string) {
-	content, err := os.ReadFile(args[0])
-	if err != nil {
-		log.Fatal("\rCouldn't read file : " + args[0])
+	if len(args) > 2 {
+		log.Fatal("Too many argument for --check.")
+	} else if args[0][len(args[0])-5:] != ".vasm" {
+		log.Fatal("Unrecognized extension for \"" + args[0] + "\", need .vasm")
 	}
 
-	var program string = string(content)
+	var program string = readFile(args[0])
 	var assemblerProgram [][]string = readProgram(program)
 
 	var startTime time.Time = time.Now()
@@ -279,11 +297,14 @@ func checkCommand(args []string) {
 	var elapsed time.Duration = time.Since(startTime)
 	if len(args) > 1 && args[1] == "-debug" {
 		fmt.Println(byteProgram)
-		fmt.Printf("Temps : %s\n\n", elapsed)
+		fmt.Printf("Time : %s\n\n", elapsed)
 	}
 }
 
-func helpCommand(_ []string) {
+func helpCommand(args []string) {
+	if len(args) > 0 {
+		log.Fatal("Too many arguments for --help.")
+	}
 	fmt.Println(`Commands:
   --run     Assemble a .vasm file and execute it with the Go implementation of the virtual machine
   --check   Check whether a .vasm file can be assembled
@@ -301,6 +322,19 @@ Command usage:
   vasm --check <file.vasm> [-debug]
   vasm --emit  <file.vasm> <output.vbc>
   vasm --load  <file.vbc> [-c-vm/-go-vm]`)
+}
+
+////////////////////
+// COMMAND HELPER //
+////////////////////
+
+func readFile(path string) string {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal("\rCouldn't read file : " + args[0])
+	}
+	var program string = string(content)
+	return program
 }
 
 /////////////////
